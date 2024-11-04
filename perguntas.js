80fe3c558e2c8e23eeadf488239b97f1bf348719
oThales-Nerd <161321915+oThales-Nerd@users.noreply.github.com>
2024-11-04 08:03:39 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,71 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Matemática',
+    'Como se calcula a área de um triângulo?',
+    'A área de um triângulo qualquer pode ser calculada, multiplicando a base pela sua altura e dividindo esse resultado por 2.'
+)
+
+criaCartao(
+    'Curiosidades',
+    'Você sabe de onde vem o aroma da baunilha?',
+    'O aroma da baunilha deriva da secreção de alguns castores'
+)
+
+criaCartao(
+    'Astronomia',
+    'Qual é o planeta mais próximo do Sol no nosso sistema solar?',
+    'É o planeta Mercúrio'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual o animal mais rápido da Terra?',
+    'O guepardo, capaz de alcançar velocidades de até 120km/h.'
+)
+
+criaCartao(
+    'Curiosidades',
+    'Qual é o idioma mais falado no mundo?',
+    'É o chinês mandarim'
+)
+
+criaCartao(
+    'Corpo humano',
+    'Qual é o maior membro do corpo humano?',
+    'É a pele'
+)
+
+criaCartao(
+    'Geografia',
+    'Quantos países existem no mundo?',
+    'Atualmente existem 193 países reconhecidos internacionalmente'
+)
+
+criaCartao(
+    'Química',
+    'Qual é o elemento mais leve do mundo?',
+    'É o hidrogênio'
+)
